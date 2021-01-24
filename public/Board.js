51a736be4f018780cd0e22467e4141a4c3fb2ecb
IsmailElShinnawy <ismailelshennawy@gmail.com>
2021-01-24 03:38:08 +0200

    added maze generation

--- public/Board.js
+++ public/Board.js
@@ -1,5 +1,6 @@
 const size = 25;
 const cells = []; //0 -> empty, 1 -> wall, 2 -> weight, 3 -> start, 4 -> end
+const visited = [];
 const rows = Math.floor(($(window).height() - 300) / size);
 const cols = Math.floor($(window).width() / size);
 
@@ -24,6 +25,132 @@ const initialize = () => {
         tableHTML += '</tr>';
     }
     $('table#board').html(tableHTML);
+}
+
+const clearWalls = () => {
+    for (let r = 0; r < rows; ++r) {
+        for (let c = 0; c < cols; ++c) {
+            if (cells[r][c] == 1) {
+                cells[r][c] = 0;
+                $(`td[id=${r}-${c}]`).removeClass('wall');
+            }
+        }
+    }
+}
+
+const setBorders = () => {
+    for (let c = 0; c < cells[0].length; ++c) {
+        if (cells[0][c] != 3 && cells[0][c] != 4)
+            cells[0][c] = 1;
+    }
+    for (let c = 0; c < cells[cells.length - 1].length; ++c) {
+        if (cells[cells.length - 1][c] != 3 && cells[cells.length - 1][c] != 4)
+            cells[cells.length - 1][c] = 1;
+    }
+    for (let r = 1; r < cells.length - 1; ++r) {
+        if (cells[r][0] != 3 && cells[r][0] != 4)
+            cells[r][0] = 1;
+    }
+    for (let r = 1; r < cells.length - 1; ++r) {
+        if (cells[r][cells[r].length - 1] != 3 && cells[r][cells[r].length - 1] != 4)
+            cells[r][cells[r].length - 1] = 1;
+    }
+}
+
+const setGrid = () => {
+    for (let r = 1; r < rows - 1; ++r) {
+        for (let c = 1; c < cols - 1; ++c) {
+            if (cells[r][c] != 3 && cells[r][c] != 4) {
+                if (r % 2 == 0 || c % 2 == 0) {
+                    cells[r][c] = 1;
+                }
+            }
+        }
+    }
+}
+
+const setMaze = () => {
+    setBorders();
+    setGrid();
+    for (let r = 0; r < rows; ++r) {
+        visited[r] = [];
+        for (let c = 0; c < cols; ++c) {
+            visited[r][c] = false;
+        }
+    }
+    generateMaze(cols + 1);
+}
+
+const generateMaze = (idx) => {
+    const neighbors = [];
+    const row = Math.floor(idx / cols);
+    const col = idx % cols;
+    visited[row][col] = true;
+    if (col > 1) {
+        if (cells[row][col - 2] != 1 && !visited[row][col - 2]) {
+            neighbors.push(get1DIdx(row, col - 2));
+        }
+    }
+    if (col < cols - 2) {
+        if (cells[row][col + 2] != 1 && !visited[row][col + 2]) {
+            neighbors.push(get1DIdx(row, col + 2));
+        }
+    }
+    if (row > 1) {
+        if (cells[row - 2][col] != 1 && !visited[row - 2][col]) {
+            neighbors.push(get1DIdx(row - 2, col));
+        }
+    }
+    if (row < rows - 2) {
+        if (cells[row + 2][col] != 1 && !visited[row + 2][col]) {
+            neighbors.push(get1DIdx(row + 2, col));
+        }
+    }
+    shuffleArray(neighbors);
+    if (neighbors.length == 0) {
+        return;
+    } else {
+        neighbors.forEach(neighbor => {
+            if (!visited[Math.floor(neighbor / cols)][neighbor % cols]) {
+                removeWallBetween(idx, neighbor);
+                generateMaze(neighbor);
+            }
+        });
+    }
+
+}
+
+const removeWallBetween = (idx1, idx2) => {
+    const row1 = Math.floor(idx1 / cols);
+    const row2 = Math.floor(idx2 / cols);
+    const col1 = idx1 % cols;
+    const col2 = idx2 % cols;
+    if (col1 > col2) {
+        if (cells[row1][col1 - 1] != 3 && cells[row1][col1 - 1] != 4)
+            cells[row1][col1 - 1] = 0;
+    } else if (col1 < col2) {
+        if (cells[row1][col2 - 1] != 3 && cells[row1][col2 - 1] != 4)
+            cells[row1][col2 - 1] = 0;
+    } else if (row1 > row2) {
+        if (cells[row1 - 1][col1] != 3 && cells[row1 - 1][col1] != 4)
+            cells[row1 - 1][col1] = 0;
+    } else if (row1 < row2) {
+        if (cells[row2 - 1][col1] != 3 && cells[row2 - 1][col1] != 4)
+            cells[row2 - 1][col1] = 0;
+    }
+}
+
+const shuffleArray = (array) => {
+    for (var i = array.length - 1; i > 0; i--) {
+        var j = Math.floor(Math.random() * (i + 1));
+        var temp = array[i];
+        array[i] = array[j];
+        array[j] = temp;
+    }
+}
+
+const get1DIdx = (row, col) => {
+    return row * cols + col;
 }
 
 const addEventListeners = () => {
--- public/index.js
+++ public/index.js
@@ -1,4 +1,23 @@
 $(document).ready(() => {
     initialize();
     addEventListeners();
+
+    $('button#generate-maze').click(() => {
+        clearWalls();
+        setMaze();
+        animateMazeBorder(0);
+    });
+
 });
+
+const animateMazeBorder = (idx) => {
+    setTimeout(() => {
+        if (idx == cells.length * cells[0].length) {
+            return;
+        }
+        if (cells[Math.floor(idx / cols)][idx % cols] == 1) {
+            $(`td[id=${Math.floor(idx / cols)}-${idx % cols}]`).addClass('wall');
+        }
+        animateMazeBorder(idx + 1);
+    }, 0);
+}
