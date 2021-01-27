22f097a14ac337fcae816c4b57070199ac5debf8
IsmailElShinnawy <ismailelshennawy@gmail.com>
2021-01-27 18:57:08 +0200

    fixed relative file paths

--- app.js
+++ app.js
@@ -2,7 +2,8 @@ const express = require('express');
 
 
 const app = express();
-app.use(express.static('public'));
+// app.use(express.static('public'));
+app.use('/public', express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.sendFile('./index.html', { root: __dirname });
